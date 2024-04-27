fc04d9fa0e732dd0b0e09b1fde14a66042ba8740
ilobilo <ilia.chachanidze35@gmail.com>
2024-04-28 01:44:37 +0400

    rewrite

---
+++ source/tetris.hpp
@@ -0,0 +1,75 @@
+// Copyright (C) 2024 ilobilo
+
+#pragma once
+
+#include <pieces.hpp>
+#include <term.hpp>
+
+#include <cstddef>
+
+#include <random>
+#include <array>
+
+namespace tetris
+{
+    inline constexpr std::size_t hsquares = 10;
+    inline constexpr std::size_t vsquares = 20;
+
+    enum class movements : std::uint8_t
+    {
+        left, right,
+        down, rotate
+    };
+
+    class board_t
+    {
+        private:
+        terminal &term;
+
+        std::array<std::array<bool, hsquares>, vsquares> buffer;
+        piece current_piece;
+        bool falling;
+
+        std::random_device rd;
+        std::mt19937_64 mt;
+        std::uniform_int_distribution<> dist;
+
+        std::size_t history[4];
+        std::size_t random();
+
+        public:
+        board_t(terminal &term) :
+            term(term), buffer { }, current_piece { 0, 0 }, falling { false },
+            rd { }, mt { rd() }, dist { 0, 34 }, history { 7, 0, 0, 0 } { }
+
+        bool is_falling() const { return this->falling; }
+
+        void add_piece();
+        std::size_t clearlines();
+
+        bool move(movements mv);
+
+        bool check(const piece pc) const;
+        void draw(std::string_view title) const;
+    };
+
+    class game
+    {
+        private:
+        terminal term;
+        board_t board;
+
+        std::size_t level;
+        std::size_t lines;
+
+        public:
+        game() : term { }, board { term }, level { 1 }, lines { 0 } { }
+
+        void start();
+
+        std::pair<std::size_t, std::size_t> get_score() const
+        {
+            return { this->level, this->lines };
+        }
+    };
+} // namespace tetris
